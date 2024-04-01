fc456d06acf5e0d6820935edae9badfd8824591a
PremPremPremPrem <prem.sy1952@gmail.com>
2024-04-01 22:27:13 +0700

    Inintial commit

---
+++ main.js
@@ -0,0 +1,71 @@
+document.querySelector('#heart-1').appendChild(new MinoBoard(
+    "__________" +
+    "_T________" +
+    "TTT____LLL" +
+    "IIIISS_LOO" +
+    "_ZZSS__JOO" +
+    "__ZZ___JJJ"
+))
+
+document.querySelector('#pc-1').appendChild(new MinoBoard(
+    "ILLLJTSSOO" +
+    "IGZZTTJJOO" +
+    "GGGZZTJGGG" +
+    "GGGGGGJGGG" +
+    "IGGGGSSGGG" +
+    "ILGGJJJGGG"
+))
+
+document.querySelector('#pc-2').appendChild(new MinoBoard(
+    "ILLLTTTZOO" +
+    "IGTTTTZZOO" +
+    "GGGTJJJGGG" +
+    "GGGGGGJGGG" +
+    "IGGGGSSGGG" +
+    "ILGGSSZGGG"
+))
+
+document.querySelector('#pc-3').appendChild(new MinoBoard(
+    "ILLLTLLLOO" +
+    "IGZZTTJJOO" +
+    "GGGZZSJGGG" +
+    "GGGGGGJGGG" +
+    "IGGGGSSGGG" +
+    "ILGGTLSGGG"
+))
+
+document.querySelector('#pc-4').appendChild(new MinoBoard(
+    "ILLLSTTTOO" +
+    "IGZZSSJJOO" +
+    "GGGZZSJGGG" +
+    "GGGGGGJGGG" +
+    "IGGGGTTGGG" +
+    "ILGGTTTGGG"
+))
+
+document.querySelector('#pc-5').appendChild(new MinoBoard(
+    "ILLLJJJLOO" +
+    "IGTTTLLLOO" +
+    "GGGTJJJGGG" +
+    "GGGGGGJGGG" +
+    "IGGGGSSGGG" +
+    "ILGGSSJGGG"
+))
+
+document.querySelector('#pc-6').appendChild(new MinoBoard(
+    "ILLLJJJJOO" +
+    "IGZZTTTJOO" +
+    "GGGZZLLGGG" +
+    "GGGGGGLGGG" +
+    "IGGGGTLGGG" +
+    "ILGGJJJGGG"
+))
+
+document.querySelector('#pc-7').appendChild(new MinoBoard(
+    "ILLLJTTTOO" +
+    "IGZZLLJJOO" +
+    "GGGZZLJGGG" +
+    "GGGGGGJGGG" +
+    "IGGGGLTGGG" +
+    "ILGGJJJGGG"
+))
